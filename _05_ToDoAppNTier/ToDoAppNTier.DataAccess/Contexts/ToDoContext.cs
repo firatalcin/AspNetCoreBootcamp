3a75c3cf34bbf61b3d5da6de21d2e5f6155904d6
Fırat Alçın <firatalcin04@gmail.com>
2024-07-15 00:47:48 +0300

    Entities

---
+++ _05_ToDoAppNTier/ToDoAppNTier.DataAccess/Contexts/ToDoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAppNTier.DataAccess.Configurations;
+using ToDoAppNTier.Entities.Concrete;
+
+namespace ToDoAppNTier.DataAccess.Contexts
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new WorkConfiguration());
+        }
+
+        public DbSet<Work> Works { get; set; }
+    }
+}
