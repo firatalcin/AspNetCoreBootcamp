b7deb7cf28338194da8ac7800047ff7665f165b1
Fırat Alçın <firatalcin04@gmail.com>
2024-07-20 22:42:21 +0300

    Repository Yazılması

---
+++ _05_ToDoAppNTier/ToDoAppNTier.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppNTier.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        Task<List<T>> GetAll(Expression<Func<T, bool>> filter=null);
+        Task<T> GetById(int id);
+        Task<T> GetByFilter(Expression<Func<T, bool>> filter, bool asNoTracking = false);
+        Task Create(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ _05_ToDoAppNTier/ToDoAppNTier.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAppNTier.DataAccess.Contexts;
+using ToDoAppNTier.DataAccess.Interfaces;
+
+namespace ToDoAppNTier.DataAccess.Repositories
+{
+    internal class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly ToDoContext _context;
+
+        public Repository(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public async Task<List<T>> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            return filter == null ?
+                await _context.Set<T>().AsNoTracking().ToListAsync() :
+                await _context.Set<T>().Where(filter).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> GetByFilter(Expression<Func<T, bool>> filter, bool asNoTracking = false)
+        {
+            return 
+                asNoTracking == true ? 
+                await _context.Set<T>().AsNoTracking().FirstOrDefaultAsync(filter) :
+                await _context.Set<T>().FirstOrDefaultAsync(filter);
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+       
+    }
+}
