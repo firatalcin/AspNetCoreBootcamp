f951e79d1d81875e796c487f11d2c18e3b16daa9
Fırat Alçın <firatalcin04@gmail.com>
2024-07-29 23:06:51 +0300

    Response 2

---
+++ _05_ToDoAppNTier/ToDoAppNTier.Common/ResponseObjects/ResponseData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppNTier.Common.ResponseObjects
+{
+    public class ResponseData<T> : Response, IResponseData<T>
+    {
+        public T Data { get; set; }
+        public ResponseData(ResponseType responseType, T data) : base(responseType)
+        {
+            Data = data;
+        }
+
+        public ResponseData(ResponseType responseType, string message) : base(responseType, message)
+        {
+
+        }
+
+        public ResponseData(ResponseType responseType, T data, List<CustomValidationError> errors) : base(responseType)
+        {
+            Data = data;
+            Data = data;
+        }
+
+        public List<CustomValidationError> ValidationErrors { get; set; }
+    }
+}
