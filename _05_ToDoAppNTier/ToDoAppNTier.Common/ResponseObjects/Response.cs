82fffc385fead7e5d8faa9c09fa587fbd6fdea6d
Fırat Alçın <firatalcin04@gmail.com>
2024-07-29 22:58:06 +0300

    Response 1

---
+++ _05_ToDoAppNTier/ToDoAppNTier.Common/ResponseObjects/Response.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppNTier.Common.ResponseObjects
+{
+    public class Response : IResponse
+    {
+        public Response(ResponseType responseType)
+        {
+            ResponseType = responseType;
+        }
+
+        public Response(ResponseType responseType, string message)
+        {
+            Message = message;
+            ResponseType = responseType;
+        }
+
+        public string Message { get; set; }
+        public ResponseType ResponseType { get; set; }
+    }
+
+    public enum ResponseType
+    {
+        Success,
+        ValidationError,
+        NotFound
+    }
+}
