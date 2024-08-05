ca21dc4ca3def49a0876ffcdbb5cca925b60f969
Fırat Alçın <firatalcin04@gmail.com>
2024-08-05 23:19:15 +0300

    Identity Kurulumu 1

---
+++ _06_IdentityProject/_06_IdentityProject.Web/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace _06_IdentityProject.Web.Contexts
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() : base()
+        {
+            
+        }
+    }
+}
---
+++ _06_IdentityProject/_06_IdentityProject.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using _06_IdentityProject.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _06_IdentityProject.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
