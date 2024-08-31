5476216e5c950c8ff6e6ad528eaa4f254d331e37
Fırat Alçın <firatalcin04@gmail.com>
2024-09-01 00:00:48 +0300

    API Consume

---
+++ _07_WebAPI/ConsumerProject/ResponseModels/ProductResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsumerProject.ResponseModels
+{
+    public class ProductResponseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public int? CategoryId { get; set; }
+    }
+}
