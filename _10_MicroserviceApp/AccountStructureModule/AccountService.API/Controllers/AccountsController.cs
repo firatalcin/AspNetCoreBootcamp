2e91a6803db5822e431ef18242e07fce24bce024
Fırat Alçın <firatalcin04@gmail.com>
2024-12-25 23:39:21 +0300

    Account Structure 3

--- _10_MicroserviceApp/AccountStructureModule/AccountService.API/Controllers/AccountsController.cs
+++ _10_MicroserviceApp/AccountStructureModule/AccountService.API/Controllers/AccountsController.cs
@@ -18,10 +18,9 @@ namespace AccountService.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
-        {
-            var repository = new PlayerRepository();
-            return Ok();
+        public async Task<IActionResult> Get()
+        {            
+            return Ok(await _playerRepository.GetAll());
         }
 
         [HttpPost]
@@ -35,6 +34,28 @@ namespace AccountService.API.Controllers
             });
 
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _playerRepository.GetById(id);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _playerRepository.Remove(id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(PlayerUpdateDto dto)
+        {
+            await _playerRepository.Update(new Data.Entities.Player { FirstName = dto.FirstName, LastName = dto.LastName, Username = dto.Username, Id = dto.Id });
+
+            return NoContent();
         }
     }
 }
