1d69d21907e7f43fde0f4cda4dfd26d0fe5f7cfc
Fırat Alçın <firatalcin04@gmail.com>
2025-01-01 17:55:27 +0300

    ProductList

---
+++ ST_Bootcamp/StoreApp/StoreApp.Data/Concrete/EfStoreRepository.cs
@@ -0,0 +1,18 @@
+﻿using StoreApp.Data.Abstract;
+
+namespace StoreApp.Data.Concrete;
+
+public class EfStoreRepository : IStoreRepository
+{
+    private StoreDbContext _context;
+    public EfStoreRepository(StoreDbContext context)
+    {
+        _context = context;
+    }
+    public IQueryable<Product> Products => _context.Products;
+
+    public void CreateProduct(Product entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ST_Bootcamp/StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Data.Abstract;
+using StoreApp.Web.Models;
+
+namespace StoreApp.Web.Controllers;
+
+public class HomeController :Controller
+{
+    private IStoreRepository _storeRepository;
+    public HomeController(IStoreRepository storeRepository)
+    {
+        _storeRepository = storeRepository;
+    }
+    public IActionResult Index() {
+      
+        var products = _storeRepository.Products.Select(p => new ProductViewModel {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            Price = p.Price
+        }).ToList();
+
+        return View(new ProductListViewModel {
+            Products = products
+        });
+
+    }
+}
